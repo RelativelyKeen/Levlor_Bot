c7d8b9919b1c4b90c75cfe693f2f81b5ad2b303c
RelativelyKeen <71721535+RelativelyKeen@users.noreply.github.com>
2022-04-21 10:26:34 -0700

    updated everything

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from unittest import expectedFailure
 from dotenv import load_dotenv
 import discord, json, os, random
 from datetime import datetime, timedelta
@@ -127,7 +128,25 @@ def get_ranks(guild, user):
             for key, value in x.items():
                 if value==sortedKey:
                     y[key]=value
-        return Reverse(list(y.keys()))
+    return Reverse(list(y.keys()))
+    
+def get_levels(guild):
+    with open(os.path.join(f'{dir}' , 'ranks', 'points', f'{guild}.json'), 'r') as f:
+        x = json.load(f)
+        y={}
+
+        sortedList=sorted(x.values())
+
+        for sortedKey in sortedList:
+            for key, value in x.items():
+                if value==sortedKey:
+                    y[key]=value
+    pointsinlevel = get_setting(guild, "points")
+    pointslist = Reverse(list(y.values()))
+    levels = []
+    for i in pointslist:
+        levels.append(i//pointsinlevel)
+    return levels
     
 bot = commands.Bot(command_prefix=(get_prefix_start))
 bot.remove_command("help") #makes it so my custom help command works
@@ -139,19 +158,42 @@ usertimes = {}
 async def on_message(message):
     if (message.author.bot):
         return
+    
     global lastauthor
     global usertimes
+    
     if not lastauthor:
         lastauthor = message.author.id
         return
     if message.author.id == lastauthor:
         return
+    
     now = datetime.now()
     more = now + timedelta(0, random.randint(7, 12))
     usertimes.update({message.author.id : more.strftime("%H:%M:%S")})
     if datetime.now() > datetime.strptime(usertimes[message.author.id], "%H:%M:%S"):
         add_points(message.guild.id, message.author.id, 1)
-        
+    
+    pointsforlevel = get_setting(message.guild.id, "points")
+    points = get_points(message.guild.id, message.author.id)
+    if points == 0:
+        return
+    elif points % pointsforlevel == 0:
+        await message.reply(f"<@{message.author.id}> Just leveled up to level {points // pointsforlevel}!")
+        if discord.utils.get(message.guild.roles, name=f"Level {points // pointsforlevel}"):
+            role = discord.utils.get(message.guild.roles, name=f"Level {points // pointsforlevel}")
+            oldrole = discord.utils.get(message.guild.roles, name=f"Level {points // pointsforlevel-1}")
+            user = message.author
+            await user.add_roles(role)
+            await user.remove_roles(oldrole)
+        else:
+            await message.guild.create_role(name=f"Level {points // pointsforlevel}", colour=discord.Colour(0xFFD700))
+            role = discord.utils.get(message.guild.roles, name=f"Level {points // pointsforlevel}")
+            oldrole = discord.utils.get(message.guild.roles, name=f"Level {points // pointsforlevel-1}")
+            user = message.author
+            await user.add_roles(role)
+            await user.remove_roles(oldrole)
+    
     lastauthor = message.author.id
 
 @bot.event
@@ -164,18 +206,32 @@ async def on_guild_join(guild):
     joinguild(guild.id)
     
 @bot.command()
-async def level(ctx):
-    pointsforlevel = get_setting(ctx.guild.id, "points")
-    points = get_points(ctx.guild.id, ctx.author.id)
-    level = points // pointsforlevel
-    pointsleft = points % pointsforlevel
-    author = ctx.message.author
-    pfp = author.avatar_url
-    rank = get_rank(ctx.guild.id, ctx.author.id)
+async def level(ctx, user: discord.User=None):
+    if not user:
+        pointsforlevel = get_setting(ctx.guild.id, "points")
+        try:
+            points = get_points(ctx.guild.id, ctx.author.id)
+        except TypeError:
+            points = get_points(ctx.guild.id, ctx.author.id)
+        level = points // pointsforlevel
+        pointsleft = points % pointsforlevel
+        author = ctx.message.author
+        pfp = author.avatar_url
+        rank = get_rank(ctx.guild.id, ctx.author.id)
+    elif user.bot:
+        await ctx.send("User is a bot")
+    else:
+        pointsforlevel = get_setting(ctx.guild.id, "points")
+        points = get_points(ctx.guild.id, user.id)
+        level = points // pointsforlevel
+        pointsleft = points % pointsforlevel
+        author = user
+        pfp = author.avatar_url
+        rank = get_rank(ctx.guild.id, user.id)
     await ctx.send(embed=discord.Embed.from_dict({
       "title": f"**Rank:** #{rank}",
       "color": 0,
-      "description": f"**Level:** {level}\n\n*Points left:* {pointsleft}/{pointsforlevel}",
+      "description": f"**Level:** {level}\n\n*Points:* {pointsleft}/{pointsforlevel}",
       "timestamp": "",
       "author": {
         "name": f"{author}",
@@ -189,6 +245,7 @@ async def level(ctx):
 
 @bot.command()
 async def leaderboard(ctx):
+    levels = get_levels(ctx.guild.id)
     ranks = get_ranks(ctx.guild.id, ctx.author.id)
     names = []
     for i in ranks:
@@ -199,10 +256,15 @@ async def leaderboard(ctx):
             break
         else:
             names.append("Nobody")
+    while True:
+        if len(levels) >= 10:
+            break
+        else:
+            levels.append(0)
     await ctx.send(embed=discord.Embed.from_dict({
       "title": "**Leaderboard**",
       "color": 0,
-      "description" : f"**1.** {names[0]}\n**2.** {names[1]}\n**3.** {names[2]}\n**4.** {names[3]}\n**5.** {names[4]}\n**6.** {names[5]}\n**7.** {names[6]}\n**8.** {names[7]}\n**9.** {names[8]}\n**10.** {names[9]}",
+      "description" : f"**1.** {names[0]} - *Level: *{levels[0]}\n**2.** {names[1]} - *Level: *{levels[1]}\n**3.** {names[2]} - *Level: *{levels[2]}\n**4.** {names[3]} - *Level: *{levels[3]}\n**5.** {names[4]} - *Level: *{levels[4]}\n**6.** {names[5]} - *Level: *{levels[5]}\n**7.** {names[6]} - *Level: *{levels[6]}\n**8.** {names[7]} - *Level: *{levels[7]}\n**9.** {names[8]} - *Level: *{levels[8]}\n**10.** {names[9]} - *Level: *{levels[9]}",
       "timestamp": "",
       "image": {},
       "thumbnail": {},
@@ -260,9 +322,6 @@ async def settings(ctx, setting, args):
         await ctx.send(f'Prefix changed to: {args}')
     elif setting == "prefix" and args and len(args) >= 5:
         await ctx.send("Choosen prefix too long")  
-    if setting == "points" and int(args) > 0:
-        set_setting(ctx.guild.id, "points", int(args))
-        await ctx.send(f'Amount of points in a level changed to: {str(args)}')
         
         
 #settings help message if there is no args
@@ -282,10 +341,6 @@ async def flip_error(ctx, error):
                {
                  "name": "prefix",
                  "value": f"used to change bot prefix\nusage: {prefix}settings prefix <new prefix>"
-               },
-               {
-                 "name": "Points",
-                 "value": f"used to change the amount of points for a levle\nusage: {prefix}settings points <new amount>"
                }
              ]
            }))
